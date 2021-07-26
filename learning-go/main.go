58c7e5181e9951ea927fad89125849754e928903
foroozf001 <farazforoozan@gmail.com>
2021-07-26 17:42:03 +0200

    Added section on pointers

--- learning-go/main.go
+++ learning-go/main.go
@@ -1,8 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
+	// section_VariablesAndFunctions()
+	// section_Pointers()
+}
+
+func section_VariablesAndFunctions() {
 	fmt.Println("Hello, world!")
 	var whatToSay string
 	var i int
@@ -16,4 +24,18 @@ func main() {
 
 func saySomething() string {
 	return fmt.Sprint("Something")
+}
+
+func section_Pointers() {
+	var myString string
+	myString = "Green"
+	log.Println("myString is set to", myString)
+	changeUsingPointer(&myString)
+	log.Println("myString is set to", myString)
+}
+
+func changeUsingPointer(s *string) {
+	log.Println("s is set to", s)
+	newValue := "Red"
+	*s = newValue
 }
