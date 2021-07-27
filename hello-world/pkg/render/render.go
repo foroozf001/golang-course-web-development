891e7581b54b36d24c7164c7baf41abb358d7c01
foroozf001 <farazforoozan@gmail.com>
2021-07-27 14:16:24 +0200

    Built template cache for pages

--- hello-world/pkg/render/render.go
+++ hello-world/pkg/render/render.go
@@ -1,21 +1,63 @@
 package render
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
 	"text/template"
 )
 
+var functions = template.FuncMap{}
+
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, err := template.ParseFiles("../../templates/" + tmpl)
+	var parsedTemplate *template.Template
+	// var templateCache map[string]*template.Template
+	var err error
+
+	_, err = RenderTemplateTest(w)
 	if err != nil {
-		log.Println("error render.go:", err)
+		log.Println("error render templatetest:", err)
+	}
+	parsedTemplate, err = template.ParseFiles("../../templates/" + tmpl)
+	if err != nil {
+		log.Println("error render parsefiles:", err)
 		return
 	}
 	err = parsedTemplate.Execute(w, nil)
 	if err != nil {
-		log.Println("error render.go:", err)
-		return
+		log.Println("error render execute:", err)
 	}
-	log.Println("HTTP 200 OK")
+}
+
+func RenderTemplateTest(w http.ResponseWriter) (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+	pages, err := filepath.Glob("../../templates/*.page.tmpl.html")
+	if err != nil {
+		log.Println("error render pages:", err)
+		return myCache, nil
+	}
+	for _, page := range pages {
+		name := filepath.Base(page)
+		fmt.Println("Page is currently", page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			log.Println("error render parse pages:", err)
+			return myCache, nil
+		}
+		matches, err := filepath.Glob("../../templates/*.layout.tmpl.html")
+		if err != nil {
+			log.Println("error render matches:", err)
+			return myCache, nil
+		}
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("../../templates/*.layout.tmpl.html")
+			if err != nil {
+				log.Println("error render parse layout:", err)
+				return myCache, nil
+			}
+		}
+		myCache[name] = ts
+	}
+	return myCache, nil
 }
